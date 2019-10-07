7960f4dda9c2f5824cbed5e502d3bfa773d95e44
Blake Andrew Price <blakeandrewprice@gmail.com>
2019-10-07 14:43:42 -0700

    Storyboard step 5 - 8: Added text field and text view. Basic constraints added, will probably adjust. Added Save button. Created BookDetailVC and set class. Added text field/view outlets and save button action.

---
+++ Reading List/Reading List/BookDetailViewController.swift
@@ -0,0 +1,22 @@
+//
+//  BookDetailViewController.swift
+//  Reading List
+//
+//  Created by Blake Andrew Price on 10/7/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class BookDetailViewController: UIViewController {
+    //MARK: - Outlets
+    @IBOutlet weak var bookTitleTextField: UITextField!
+    @IBOutlet weak var bookTextView: UITextView!
+    //MARK: - Actions
+    @IBAction func saveBookButtonPressed(_ sender: UIBarButtonItem) {
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
